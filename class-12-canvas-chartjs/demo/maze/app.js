f329c9996fe2a43c6e483433b48c2b4fbed80691
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-05-16 07:27:09 -0700

    update class 12

--- class-12-canvas-chartjs/demo/maze/app.js
+++ class-12-canvas-chartjs/demo/maze/app.js
@@ -22,22 +22,38 @@ maze.move();
 /*  all together now! 
 
 write four functions
-each function will console out a line from the following stanza
+each function will console.log a line from the following stanza
 
     She says, we've got to hold on to what we've got
-    'Cause it doesn't make a difference if we make it or not
+    Cause it doesn't make a difference if we make it or not
     We've got each other and that's a lot for love
     We'll give it a shot
 
 call the functions manually in your browser console
 singLineOne(); // "She say's we've got to hold on to what we've got"
 
+
+
+
+
+
+
 now call the next line's function from the one that comes before it
 so when you call the first function, it will print the whole stanza
 singLineThree(); // "We've got each other and that's a lot for love
                      We'll give it a shot"
 
-now move the functions out of order - just for the fun of it   
+
+
+
+
+
+
+
+
+bonus round! choose one
+1. add all functions to an object called "song" - how do you "play" the song?
+2. move the functions out of order - just for the fun of it   
 do they still work? what if you call them in between creating them instead of from the console?
 what if you change them from function declarations to function expressions?                 
 
--- class-12-canvas-chartjs/demo/tickets/app.js
+++ class-12-canvas-chartjs/demo/tickets/app.js
@@ -1,31 +1,25 @@
 'use strict'
 
 var context = document.getElementById( 'squares' ).getContext( '2d' );
-// draw squares on the canvas
 
-var flightChart = new Chart ( context, {
-    type: 'line',
-    data: {
-        labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ],
-        datasets: [
-            {
-                label: 'spirit airlines',
-                data: [500,330,414,153,135,532,124,234,234,234,234,523],
-                backgroundColor: 'rgba(200,20,20,.5)'
-            },
-            {
-                label: 'american airlines',
-                data: [300,338,414,333,333,332,324,234,234,234,234,323],
-                backgroundColor: 'rgba(20,200,20,.5)'
-            },
-            {
-                label: 'singapore airlines',
-                data: [100,120,245,122,155,156,600,688,132,211,433,155],
-                backgroundColor: 'rgba(20,20,200,.5)'
-            }
-        ]
-    }
-} );
+context.fillStyle = 'green';
+context.fillRect(10, 10, 500, 500);
+
+context.strokeStyle = "white";
+
+// context.beginPath();
+var startingX = randomZ( 0, 10 );
+var startingY = randomZ( 0, 10 );
+
+for ( var i = 0; i < "so tired of for loops".length; i ++ ) {
+    var movetoX = randomZ( 100, 400 );
+    var movetoY = randomZ( 100, 400 );
+    context.moveTo( startingX, startingY );
+    context.lineTo( movetoX, movetoY );
+    context.stroke();
+}
 
 
-
+function randomZ ( min, max ) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
