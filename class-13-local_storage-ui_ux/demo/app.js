99e71e16e26c0c97e089cec5ec6472562a02469f
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-05-17 13:26:52 -0700

    update class13

--- class-13-local_storage-ui_ux/demo/app.js
+++ class-13-local_storage-ui_ux/demo/app.js
@@ -1,3 +1,34 @@
+// TODO save submissions in LocalStorage so I can refresh the page and not lose my list
+
+// on load get saved submissions and add to the list
+var submissions = JSON.parse( localStorage.getItem( 'submissions' ) ) || [];
+if ( submissions ) {
+    submissions.forEach( function ( submission ) {
+        addToList( submission );
+    });
+}
+
+/* using a separate array
+
+    var submissions = [];
+    var savedSubmissions = JSON.parse( localStorage.getItem( 'submissions' ) );
+    if ( savedSubmissions ) {
+        // OPTION 1 set our submissions array to our save submissions data
+        submissions = savedSubmissions; 
+
+        submissions.forEach( function ( submission ) {
+            addToList( submission );
+            // OPTION 2 add saved submissions to our submissions array and save
+            // saveToLocal( submission );
+        });
+    }
+
+
+
+*/
+
+
+
 // handle form submission
 var submissionForm = document.getElementById( 'elementsForm' );
 submissionForm.addEventListener( 'submit', submitHandler );
@@ -11,15 +42,22 @@ function submitHandler ( event ) {
 
     var submission = { name: name, ele: htmlEle };
     addToList( submission );
+    saveToLocal( submission );
 
     form.reset();
 }
 
-var list = document.querySelector( 'ul' );
+
 function addToList ( submission ) {
+    var list = document.querySelector( 'ul' );
     var newLi = document.createElement( 'li' );
+
     newLi.innerHTML = submission.name + " &#x1F60D;'s " + submission.ele;
     list.appendChild( newLi );
 }
 
-// TODO save submissions in LocalStorage so I can refresh the page and not lose my list
+function saveToLocal ( submission ) {
+    submissions.push( submission );
+    localStorage.setItem( 'submissions', JSON.stringify( submissions ) );
+}
+
